cc4555d6d0f9f8a3407a353162e815338013ae1b
Alexsey Gladkikh <alexseygladkikh@mail.ru>
2024-11-15 15:26:27 +0300

    создали функцию для преобразования даты в нужный формат

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 
 def load_file():
@@ -45,10 +46,19 @@ def get_number():
 get_number()
 
 
-def get_date(date):
+def get_date():
     """Преобразуем дату в необходимый формат
     2019-02-12T00:08:07.524972 -> 12.02.2019"""
-    pass
+    date_form = sort_info()
+    date_list = []
+    for date in date_form:
+        date_str = datetime.datetime.strptime(date['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%S:%M:%H %d-%m-%Y')
+        date_list.append((date_str[9:].replace('-', '.')))
+    date_s = '\n'.join(date_list)
+    return date_s
+
+
+get_date()
 
 
 def get_message():
